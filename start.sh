21b1cb10451200e29980e6b3bc100c7fc5afe22c
Kostiantyn Kliuiev <kliuiev@icloud.com>
2023-05-05 16:40:44 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+make down
+sleep 10
+docker system prune
+docker ps
+make up
+sleep 10
+make private_key=1ec1d7b96ee2512390f755475379448f1a0886cc3b050ed09da58e95329b2fd0 add-private-key
+sleep 10
+make add-vault-token
+sleep 10
+make generate-issuer-did-arm
+sleep 10
+make run-arm
+sleep 10
+make run-ui-arm
+sleep 10
+docker ps
